6d8f6a7c1f560ac42d6ced543a12433179f136a5
YukiHirose <db13061@stumail.daido-it.ac.jp>
2016-11-10 07:20:55 +0900

    OSVRが向いている方向へ前進するようにした

---
+++ Assets/Scripts/HeadLookWalk.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeadLookWalk : MonoBehaviour {
+	public float velocity = 0.7f;
+	public bool isWalking = false;
+
+	private CharacterController controller;
+    /*CharacterController : 衝突判定によって動きの制限を行う*/
+
+    public Clicker clicker = new Clicker();
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+	void Update () {
+        if (clicker.clicked()) {
+			isWalking = !isWalking; //クリックするたびにtrueとfalseで切り替え
+            Debug.Log(this.isWalking);
+        }
+
+		if (isWalking) {
+            // controller.SimpleMove (Camera.main.transform.forward * velocity);
+            controller.SimpleMove(Camera.main.transform.forward * velocity);
+            /*SimpleMove : キャラクターを speed 付きで動かせる
+             *Camera.main : MainCameraを参照
+             *transform.forward : オブジェクトが向いている方向(前面)のベクトルを取得(ワールド空間の Transform の青(Z)軸)
+             *velocity : 速度
+             */
+        }
+    }
+}
