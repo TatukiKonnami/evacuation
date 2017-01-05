cf61101eeb6489ba1b524d1ef222ec44869c5638
YukiHirose <db13061@stumail.daido-it.ac.jp>
2017-01-05 12:59:39 +0900

    Small_Updates

---
+++ Assets/Scripts/Leap_Finger_Acquisition.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Leap_Finger_Acquisition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
